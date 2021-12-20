5084f8bcd0cac93dfc3c94d914da929bbbe1e8fa
barmg3 <barmg3@gmail.com>
2021-12-20 21:35:59 +0200

    CounterWithColorChanging

--- app/src/main/java/com/hamza/counter/MainActivity.java
+++ app/src/main/java/com/hamza/counter/MainActivity.java
@@ -1,21 +1,54 @@
 package com.hamza.counter;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView counterText ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
-
-    public void increaseCounter(View view) {
-
+        counterText = findViewById(R.id.textView);
 
     }
+
+
+   //Onclick method form increasing the number by one and change the color every 10 numbers
+    public void increaseNumber(View view) {
+
+        String number = counterText.getText().toString();
+        int n = Integer.parseInt(number);
+        n++;
+        String a = Integer.valueOf(n).toString();
+
+        //set the number after increase it by one
+        counterText.setText(a);
+
+        //to change the color every 10 Counts
+        if (n >= 10 && n <= 19){
+            counterText.setTextColor(getResources().getColor(R.color.yallow));
+        }
+        else if (n >= 20 && n <= 29){
+            counterText.setTextColor(getResources().getColor(R.color.red));
+        }
+        else{
+            counterText.setTextColor(getResources().getColor(R.color.black));
+        }
+
+    }
+
+    //OnClick method to Reset the counter to Zero
+    public void resetNumber (View view){
+        //set the counter to Zero
+        counterText.setText("0");
+    }
+
+
 }
