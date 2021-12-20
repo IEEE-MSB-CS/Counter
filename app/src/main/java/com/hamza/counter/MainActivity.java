b19d9a602b993913722abd4ea556b9642eea5d1a
Hamza Gamal <54688005+hamza94max@users.noreply.github.com>
2021-12-20 11:53:26 +0200

    First commit

---
+++ app/src/main/java/com/hamza/counter/MainActivity.java
@@ -0,0 +1,21 @@
+package com.hamza.counter;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void increaseCounter(View view) {
+
+
+    }
+}
