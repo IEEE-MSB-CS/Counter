b669c15b33cbf4a4b73e4d8ced02ee492224c412
barmg3 <barmg3@gmail.com>
2021-12-28 20:28:10 +0200

    SplashAndArabicLanguage

--- app/src/main/java/com/hamza/counter/MainActivity.java
+++ app/src/main/java/com/hamza/counter/MainActivity.java
@@ -1,14 +1,21 @@
 package com.hamza.counter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,8 +23,12 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView countertextview;
-    int counter;
+    TextView countertextview , topScoreNumber ;
+    int counter , topScore;
+    Context context;
+    Resources resources;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +36,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         countertextview = findViewById(R.id.counterTextview);
+        topScoreNumber = findViewById(R.id.topScoreNumber);
+
 
         SharedPreferences prefs = this.getSharedPreferences("myPrefsKey", Context.MODE_PRIVATE);
-        counter = prefs.getInt("key", 0);
-        countertextview.setText(String.valueOf(counter));
+        topScore = prefs.getInt("key", 0);
+        topScoreNumber.setText(String.valueOf(topScore));
 
     }
 
@@ -43,8 +56,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
 
         switch (item.getItemId()) {
-            case R.id.Reset:
-                resetCounter();
+            case R.id.resetTopScore:
+                resetTopScore();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -56,16 +69,21 @@ public class MainActivity extends AppCompatActivity {
         checkCounter();
         countertextview.setText(String.valueOf(counter));
 
+
         SharedPreferences prefs = this.getSharedPreferences("myPrefsKey", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
-        editor.putInt("key", counter);
-        editor.commit();
+
+        if(counter > topScore){
+            editor.putInt("key", counter);
+            editor.commit();
+        }
+
+
     }
 
     private void checkCounter() {
         if (counter % 10 == 0)
             countertextview.setTextColor(getRandomcolor());
-
     }
 
     private int getRandomcolor() {
@@ -74,15 +92,18 @@ public class MainActivity extends AppCompatActivity {
         return colors[random];
     }
 
-    private void resetCounter() {
+
+    public void resetTopScore() {
+       topScore = 0;
+       topScoreNumber.setText(String.valueOf(topScore));
+
+        Toast.makeText(this, "Your Top Score Has Been Reset", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void resetCounter(View view) {
         counter = 0;
         countertextview.setText(String.valueOf(counter));
         countertextview.setTextColor(Color.BLACK);
     }
-
-    /*public void resetCounter(View view) {
-        counter = 0;
-        countertextview.setText(String.valueOf(counter));
-        countertextview.setTextColor(Color.BLACK);
-    }*/
 }
---
+++ app/src/main/java/com/hamza/counter/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.hamza.counter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreen extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        }, 2000);
+
+    }
+}
